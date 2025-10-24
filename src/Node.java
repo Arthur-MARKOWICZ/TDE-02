53010a488a28ef934ec87cb7a7f567f36a173fd8
Your Name <arthurlopes25072005@gmail.com>
2025-10-24 10:07:17 -0300

    feat: criando a classe node

---
+++ src/Node.java
@@ -0,0 +1,10 @@
+public class Node {
+    private String valor;
+    private Node esquerda;
+    private Node direita;
+    public Node(String valor){
+        this.valor = valor;
+        this.esquerda = null;
+        this.direita = null;
+    }
+}
